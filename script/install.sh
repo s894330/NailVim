30770e11bac161a57c8811cf41264c241b319d8d
nail.hsiao <nail.hsiao@acer.com>
2013-12-20 16:47:42 +0800

    update install script

--- script/install.sh
+++ script/install.sh
@@ -3,48 +3,57 @@
 #	1. install nail customization vim environment
 
 nailVim=NailVim
+RM="rm -rf"
+CP="cp -a"
+CD="cd"
+TARX="tar -xf"
+LN="ln -s"
+MKDIR="mkdir"
 
-echo -n "Setup vim..."
+echo "Setup vim..."
 
 # backup user original setting first
+echo "backup .vimrc"
 if [ -f ~/.vimrc ]; then
-	cp ~/.vimrc ~/.vimrc_backup
+	$RM ~/.vimrc_backup
+	$CP ~/.vimrc ~/.vimrc_backup
 fi
 
+echo "backup .vim"
 if [ -d ~/.vim ]; then
-	cp -a ~/.vim ~/.vim_backup
+	$RM ~/.vim_backup
+	$CP ~/.vim ~/.vim_backup
 fi
-rm -rf ~/.vimrc ~/.vim
+$RM ~/.vimrc ~/.vim
 
 # extract bundle files
-cd $nailVim
-tar -xf zipFile/bundle.tar.bz2
-cd ..
+echo "extract bundle files"
+$CD $nailVim
+$TARX zipFile/bundle.tar.bz2
+$CD ..
 
 # setup ~/.vim
-cp -a $nailVim ~/.vim
+$CP $nailVim ~/.vim
 
 # setup ~/.vimrc
-ln -s ~/.vim/vimrc ~/.vimrc
+$LN ~/.vim/vimrc ~/.vimrc
 
 # setup ~/.bin
 if [ ! -d ~/.bin ]; then
-	mkdir ~/.bin
+	$MKDIR ~/.bin
 fi
 
 for file in ~/.vim/script/script/*
 do
 	if [ ! -L ~/.bin/$(basename $file) ]; then
 		echo "link file: $(basename $file)"
-		ln -s ~/.vim/script/script/$(basename $file) ~/.bin/$(basename $file)
+		$LN ~/.vim/script/script/$(basename $file) ~/.bin/$(basename $file)
 	fi
 done
 
-echo "done"
-
-echo -n "Setup environment..."
+echo "Setup environment..."
 # setup ~/.bashrc and root bashrc
 cat $nailVim/script/alias >> ~/.bashrc
 #sudo cat $nailVim/script/rootAlias >> /etc/bash.bashrc
-echo "done"
+echo "done!!"
 echo "please execute \"source ~/.bashrc\" and then enjoy it !!"
