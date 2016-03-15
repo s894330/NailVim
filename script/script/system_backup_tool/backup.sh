8a9e1a6589e3f2a9d8e4f03e193d192eb5d50f71
Ting-Chih Hsiao <nail.hsiao@acer.com>
2016-03-15 17:06:07 +0800

    modify backup.sh restore.sh
    Signed-off-by: Ting-Chih Hsiao <nail.hsiao@acer.com>

--- script/script/system_backup_tool/backup.sh
+++ script/script/system_backup_tool/backup.sh
@@ -3,9 +3,6 @@
 echo "backup / ..."
 sbin/partclone.ext4 -c -d -s /dev/sda1 -o sda1Root_$1.img
 
-echo "backup /usr ..."
-sbin/partclone.ext4 -c -d -s /dev/sda5 -o sda5Usr_$1.img
-
 sync
 sync
 sync
--- script/script/system_backup_tool/restore.sh
+++ script/script/system_backup_tool/restore.sh
@@ -3,9 +3,6 @@
 echo "restore / ..."
 sbin/partclone.ext4 -r -d -s sda1Root_$1.img -o /dev/sda1
 
-echo "restore /usr ..."
-sbin/partclone.ext4 -r -d -s sda5Usr_$1.img -o /dev/sda5
-
 sync
 sync
 sync
