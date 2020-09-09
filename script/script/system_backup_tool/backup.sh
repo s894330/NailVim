23c28c2d986501004d4ce98171f479ae0ac478d7
nail.hsiao <nail.hsiao@acer.com>
2020-09-09 16:11:25 +0800

    update backup solution
    Signed-off-by: nail.hsiao <nail.hsiao@acer.com>

--- script/script/system_backup_tool/backup.sh
+++ script/script/system_backup_tool/backup.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# get root rights
+echo -n "Please enter password for sudo rights: "
+read -s pass
+echo ""
 echo "backup / ..."
-sbin/partclone.ext4 -c -d -s /dev/sda1 -o sda1Root_$1.img
+echo $pass | sudo -S partclone.ext4 -c -d -s /dev/sda1 -o sda1Root_$1.img
 
 sync
 sync
--- script/script/system_backup_tool/restore.sh
+++ script/script/system_backup_tool/restore.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# get root rights
+echo -n "Please enter password for sudo rights: "
+read -s pass
+echo ""
 echo "restore / ..."
-sbin/partclone.ext4 -r -d -s sda1Root_$1.img -o /dev/sda1
+echo $pass | sudo -S partclone.ext4 -r -d -s sda1Root_$1.img -o /dev/sda1
 
 sync
 sync
